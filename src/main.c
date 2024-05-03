84988d16b5ec07908ff5c4c21804f6ed9453609f
DavidOrtegaGarcia <daorga77@gmail.com>
2024-05-03 13:19:14 +0200

    Daily commit

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: daortega <daortega@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/24 14:01:58 by daortega          #+#    #+#             */
-/*   Updated: 2024/04/24 14:25:38 by daortega         ###   ########.fr       */
+/*   Updated: 2024/05/03 13:17:30 by daortega         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,10 +16,6 @@ int	main(int argc, char *argv[])
 {
 	if (argc != 1)
 		return(printf("Wrong number of arguments\n"), -1);
-	while (1)
-	{
-		
-				
-	}
+	
 	
 }
