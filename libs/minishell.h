95a290f74ec26555b0fa6f8d9f32c41b8e587abd
DavidOrtegaGarcia <daorga77@gmail.com>
2024-05-08 17:13:12 +0200

    Daly commit

--- libs/minishell.h
+++ libs/minishell.h
@@ -6,16 +6,18 @@
 /*   By: daortega <daortega@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/24 14:04:32 by daortega          #+#    #+#             */
-/*   Updated: 2024/04/24 14:17:03 by daortega         ###   ########.fr       */
+/*   Updated: 2024/05/08 16:38:28 by daortega         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINISHELL_H
 # define MINISHELL_H
 
-# include "../libft/libft.h"
+# include <libft.h>
 # include <stdio.h>
 # include <unistd.h>
+# include <readline/readline.h>
+# include <readline/history.h>
 
 /*---------STRUCTS----------*/
 
--- src/main.c
+++ src/main.c
@@ -6,16 +6,22 @@
 /*   By: daortega <daortega@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/24 14:01:58 by daortega          #+#    #+#             */
-/*   Updated: 2024/05/03 13:17:30 by daortega         ###   ########.fr       */
+/*   Updated: 2024/05/08 16:37:37 by daortega         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../libs/minishell.h"
+#include "minishell.h"
 
-int	main(int argc, char *argv[])
+int	main(int argc, char *argv[], char *env[])
 {
+	(void)argv;
+	(void)env;
 	if (argc != 1)
 		return(printf("Wrong number of arguments\n"), -1);
-	
-	
+	while (1)
+	{
+		char *line;
+    	line = readline("Ingresa una línea de texto: ");
+		printf("La línea ingresada es: %s\n", line);
+	}	
 }
