55ff6fd29717760d876134fd3a51b25062fdd0bb
David Ortega Garcia <daortega@car6s3.42barcelona.com>
2024-05-13 18:17:16 +0200

    Daly commit

--- src/expansor/expansor.c
+++ src/expansor/expansor.c
@@ -1,36 +1,22 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   expansor.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: daortega <daortega@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/04/24 14:01:58 by daortega          #+#    #+#             */
-/*   Updated: 2024/05/13 17:52:09 by daortega         ###   ########.fr       */
+/*   Created: 2024/05/13 17:30:43 by daortega          #+#    #+#             */
+/*   Updated: 2024/05/13 17:47:58 by daortega         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "minishell.h"
-
-int	main(int argc, char *argv[], char *env[])
+void expansor(char *line, char **env)
 {
-	char	*line;
-	t_env	*t_env;
+	int i;
 
-	(void)argv;
-	(void)env;
-	if (argc != 1)
-		return(printf("Wrong number of arguments\n"), -1);
-	t_env = fill_t_env(env);
-	while (1)
+	i = 0;
+	while (line[i] != '\0')
 	{
-    	line = readline("minishell$ ");
-		if (line != NULL && line[0] != '\0') 
-			add_history(line);
-		ft_printf("%s\n", line);
-		//parseo
-		expansor(line, env);
-		free(line);
-		
-	}
+		if ()
+	}	
 }
