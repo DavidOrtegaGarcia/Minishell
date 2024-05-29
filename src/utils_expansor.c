64c88afcde1da190ca6fab8c1749cb0c1caa826f
DavidOrtegaGarcia <daorga77@gmail.com>
2024-05-29 17:36:27 +0200

    Daly commit

--- src/utils_expansor.c
+++ src/utils_expansor.c
@@ -6,7 +6,7 @@
 /*   By: daortega <daortega@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/24 15:07:17 by daortega          #+#    #+#             */
-/*   Updated: 2024/05/28 18:01:30 by daortega         ###   ########.fr       */
+/*   Updated: 2024/05/29 14:57:17 by daortega         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,14 +21,12 @@ int	compare_key(char *line, char *key)
 	i = 0;
 	while (line[i] != '\0' && key[i] != '\0' && result == 0)
 	{
-		//printf("COMAPRE KEY: %i\n", i);
 		result = line[i] - key[i];
 		i++;
 	}
 	if ((line[i] != '\0' || key[i] != '\0') &&
 		(ft_isalpha(line[i]) == 1 || ft_isalpha(key[i]) == 1) && result == 0)
 		result = line[i] - key[i];
-	//printf("result = %i \nline[i] = %i \nkey[i] = %i\n", result, line[i], key[i]);
 	if (result > 0)
 		result = 1;
 	else if (result < 0)
