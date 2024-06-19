dfe27fec1752eea4fdad81e1bf63911c76937ba3
Ricardo Pocaterra Meza <rpocater@cbr7s1.42barcelona.com>
2024-06-19 15:57:31 +0200

    fixed address sanitizer

--- libs/minishell.h
+++ libs/minishell.h
@@ -6,7 +6,7 @@
 /*   By: daortega <daortega@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/24 14:04:32 by daortega          #+#    #+#             */
-/*   Updated: 2024/06/19 14:35:32 by rpocater         ###   ########.fr       */
+/*   Updated: 2024/06/19 15:54:41 by rpocater         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,7 +29,7 @@
 /*---------STRUCTS----------*/
 typedef struct s_token
 {
-	char			*content;
+	char		*content;
 	struct s_token	*next;
 }	t_token;
 
