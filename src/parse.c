f4a468687a1764d3c8761c1a006899e1ff432306
Ricardo Pocaterra Meza <rpocater@car11s3.42barcelona.com>
2024-07-08 17:41:38 +0200

    new version with pipes used

--- src/parse.c
+++ src/parse.c
@@ -6,7 +6,7 @@
 /*   By: rpocater <rpocater@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/14 12:52:16 by rpocater          #+#    #+#             */
-/*   Updated: 2024/05/22 12:52:31 by rpocater         ###   ########.fr       */
+/*   Updated: 2024/07/08 15:36:05 by rpocater         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,4 +29,86 @@ int	parse_input(int argc, char **argv, char **envp)
 	else
 		return (2);
 	return (0);
+}
+
+void	print_commands(t_com *com)
+{
+	int	i;
+	int	x;
+	t_com	*elem;
+
+	elem = com;
+	i = 0;
+	x = 0;
+	while (elem != NULL)
+	{
+		printf("Command %d: \n", i);
+		while (elem->command[x] != NULL)
+		{
+			printf("%s\n", elem->command[x]);
+			x++;
+		}
+		elem = elem->next;
+		i++;
+		x = 0;
+	}
+	return ;
+}
+
+char	**con_with_i(t_token *list, int x)
+{
+	char	**ret;
+	t_token	*elem;
+	int	i;
+
+	ret = (char **)malloc(sizeof(char *) * (x + 1));
+	if (ret == NULL)
+		return (NULL);
+	elem = list;
+	i = 0;
+	while (elem != NULL && i < x)
+	{
+		*(ret + i) = ft_strdup(elem->content);
+		elem = elem->next;
+		i++;
+	}
+	*(ret + i) = NULL;
+	return (ret);
+}
+
+t_com	*ft_lst_to_coms(t_token *list)
+{
+	int	i;
+	t_com	*ret;
+	t_token	*elem;
+
+	ret = NULL;
+	i = 0;
+	elem = list;
+	while (elem != NULL && elem->content[0] != '|')
+	{
+		elem = elem->next;
+		i++;
+	}
+	if (i > 0)
+	{
+		printf("I: %d\n", i);
+		ret = (t_com *)malloc(sizeof(t_com));
+		if (ret == NULL)
+			return (NULL);
+		ret->command = con_with_i(list, i);
+		if (elem != NULL)
+		{
+			if (elem->content[0] == '|')
+			{
+				elem = elem->next;
+				ret->next = ft_lst_to_coms(elem);
+			}
+		}
+		else
+			ret->next = NULL;
+	}
+	else
+		printf("Syntax error at list to command conversion\n");
+	return (ret);
 }
