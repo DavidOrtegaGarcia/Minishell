e5720e4cbcb29dee41107ecca1ffeb6ab385236b
Ricardo Pocaterra Meza <rpocater@car11s3.42barcelona.com>
2024-07-08 15:12:57 +0200

    adding .h

--- libs/minishell.h
+++ libs/minishell.h
@@ -6,7 +6,7 @@
 /*   By: daortega <daortega@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/24 14:04:32 by daortega          #+#    #+#             */
-/*   Updated: 2024/07/02 12:36:04 by rpocater         ###   ########.fr       */
+/*   Updated: 2024/07/08 15:12:23 by rpocater         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,11 +27,33 @@
 # include <signal.h>
 
 /*---------STRUCTS----------*/
+typedef enum s_redirect_type
+{
+	REDIRECT_NONE = 0,
+	IN,
+	HERE_DOC,
+	OUT_TRUNC,
+	OUT_APPEND,
+}	t_type;
+
 typedef struct s_token
 {
 	char		*content;
 	struct s_token	*next;
 }	t_token;
+
+typedef struct s_redir
+{
+	t_type		type;
+	char		*file;
+}	t_redir;
+
+typedef struct s_com
+{
+	char		**command;
+	//struct		*s_redir;
+	struct	s_com	*next;
+}	t_com;
 
 typedef struct s_env
 {
@@ -55,10 +77,12 @@ int	ft_free_list(t_token *list);
 int	ft_metachr(int c);
 t_token	*ft_tokenlast(t_token *tkn);
 void	print_list(t_token *list);
+void	print_commands(t_com *com);
 int	ft_addmetachr(char *line, int start, int x);
 int	ft_addprint(char *line, int x);
 int	ft_addend(char *line, int x);
 int	ft_addquote(char *line, int start, int x);
 char	**ft_lst_to_matrix(t_token *list);
+t_com	*ft_lst_to_coms(t_token *list);
 void	ft_free(char **str);
 #endif
