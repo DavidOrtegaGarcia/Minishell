d2bee4aa9902ed2d09138eec3d43c6564fb1d3b1
David Ortega Garcia <daortega@car7s2.42barcelona.com>
2024-07-16 16:14:57 +0200

    Daly commit

--- src/execute_dav.c
+++ src/execute_dav.c
@@ -6,120 +6,106 @@
 /*   By: daortega <daortega@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/10 14:45:03 by daortega          #+#    #+#             */
-/*   Updated: 2024/07/15 17:05:51 by daortega         ###   ########.fr       */
+/*   Updated: 2024/07/16 15:50:37 by daortega         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <minishell.h>
 
-void out_a_redir(char *file)
+void the_whatipids(t_exec exec)
 {
-	int fd;
+	int i;
+	int child_status;
 
-	fd = open(file, O_WRONLY | O_CREAT, 0644);
-	if (fd == -1)
-		return (printf(MSG_OPEN_F, file), exit(OPEN_F));
-	dup2(fd, STDOUT_FILENO);
-	close(fd);
-}
-
-void out_t_redir(char *file)
-{
-	int fd;
-
-	fd = open(file, O_WRONLY | O_TRUNC | O_CREAT, 0644);
-	if (fd == -1)
-		return (printf(MSG_OPEN_F, file), exit(OPEN_F));
-	dup2(fd, STDOUT_FILENO);
-	close(fd);
-}
-
-void hd_redir(char *file)
-{
-	int fd;
-
-	fd = open(file, O_RDONLY);
-	if (fd == -1)
-		return (printf(MSG_OPEN_F, file), exit(OPEN_F));
-	dup2(fd, STDIN_FILENO);
-	close(fd);
-}
-
-void in_redir(char* file)
-{
-	int fd;
-
-	if (access(file, F_OK ) == -1)
-		return(printf(MSG_FDE, file), exit(FDE));
-	fd = open(file, O_RDONLY);
-	if (fd == -1)
-		return (printf(MSG_OPEN_F, file), exit(OPEN_F));
-	dup2(fd, STDIN_FILENO);
-	close(fd);
-}
-
-int make_redirections(t_redir *redir)
-{
-	int fd[2];
-	
-	while (redir != NULL)
+	i = 0;
+	close(exec.fd[0]);
+	close(exec.fd[1]);
+	while(i < exec.n_com)
 	{
-		if (redir->type = IN)
-			in_redir(redir->file);
-		else if (redir->type = HERE_DOC)
-			in_redir(redir->file);
-		else if (redir->type = OUT_TRUNC)
-			out_t_redir(redir->file);
-		else if (redir->type = OUT_APPEND);
-			out_a_redir(redir->file);
-		redir = redir->next;
+		waitpid(exec.pids[i], &child_status, 0);
+		if (WIFEXITED(child_status))
+			*exec.status = WEXITSTATUS(child_status);
+		i++;
 	}
-	return (0);
+}
+void exec_command(t_com *t_command, t_env *l_env, t_exec exec, int i)
+{
+	char *path;
+
+	path = find_path(t_command->command[0], l_env);
+	if (path == NULL)
+		return(perror(MSG_CNR), exit(CNR), -1);
+	dup2(exec.fd[0], STDIN_FILENO);
+	close(exec.fd[0]);
+	if (i != exec.n_com -1) 
+		dup2(exec.fd[1], STDOUT_FILENO);
+	close(exec.fd[1]);
+	make_redirections(t_command->redir);
+	execve(path, t_command->command, exec.env);
 }
 
-void exec_one_command(t_com *command, t_env *l_env, char *env[], int *status)
+void exec_first_command(t_com *t_command, t_env *l_env, t_exec exec)
 {
-	int path;
+	char *path;
+
+	exec.pids[0] = fork();
+	if (exec.pids[0] < 0)
+		return(perror(MSG_FORK_F), exit(FORK_F));
+	if (exec.pids[0] == 0)
+	{
+		path = find_path(t_command->command[0], l_env);
+		if (path == NULL)
+			return(perror(MSG_CNR), exit(CNR));
+		close(exec.fd[0]);
+		if (exec.n_com > 1)
+			dup2(exec.fd[1], STDOUT_FILENO);
+		close(exec.fd[1]);
+		make_redirections(t_command->redir);
+		execve(path, t_command->command, exec.env);
+	}
+}
+
+/*void exec_one_command(t_com *t_command, t_env *l_env, char *env[], int *status)
+{
+	char *path;
 	int pid;
 	int child_status;
 
 	pid = fork();
 	if (pid < 0)
-		return (perror(MSG_FORK_F), exit(EXIT_FAILURE), -1);
+		return (perror(MSG_FORK_F), exit(FORK_F));
 	if (pid == 0)
 	{
-		make_redirections(command->redir);
-		path = find_path(command->command[0], l_env);
+		make_redirections(t_command->redir);
+		path = find_path(t_command->command[0], l_env);
 		if (path == NULL)
 			return(perror(MSG_CNR), exit(MSG_CNR));
-		execve(path, command, env);
+		execve(path, t_command->command, env);
 	}
 	waitpid(pid, &child_status, 0);
 	if (WIFEXITED(child_status))
-		*status = WEXITSTATUS(status);
-}
+		*status = WEXITSTATUS(child_status);
+}*/
 
-void    execute(t_com *command, t_env *l_env, char *env[], int *status)
+void    execute(t_com *t_command, t_env *l_env, char *env[], int *status)
 {
 	t_exec exec;
 	int n_com;
 	int i;
 
-	n_com = get_commands(command);
-	if (n_com == 1)
-		return (exec_one_command(command, l_env, env, status));
+	n_com = get_n_commands(t_command);
 	exec = fill_exec(env, status, n_com);
-	i = 0;
-	exec_first_command(command, l_env, exec);
+	exec_first_command(t_command, l_env, exec);
+	i = 1;
 	while(i < n_com)
 	{
 		exec.pids[i] = fork();
 		if (exec.pids[i] < 0)
 			return (perror(MSG_FORK_F), exit(EXIT_FAILURE), -1);
 		if (exec.pids[i] == 0)
-			exec_command();
+			exec_command(t_command, l_env, exec, i);
 		i++;
-		command = command->next;
+		t_command = t_command->next;
 	}
-	return (0);
+	the_whatipids(exec);
 }
