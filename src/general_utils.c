937601f444b35860a477b6132763bbc950166f70
Ricardo Pocaterra Meza <rpocater@car11s3.42barcelona.com>
2024-07-25 16:23:40 +0200

    fixed null errors

--- src/general_utils.c
+++ src/general_utils.c
@@ -6,7 +6,7 @@
 /*   By: rpocater <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/24 16:10:22 by rpocater          #+#    #+#             */
-/*   Updated: 2024/07/24 17:45:25 by rpocater         ###   ########.fr       */
+/*   Updated: 2024/07/25 16:22:44 by rpocater         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,8 +18,8 @@ t_com	*ft_token_and_parse(char *line, int *status)
 	t_com	*com;
 
 	list = ft_tokenize(line);
-	if (list == NULL)
-		exit(EXIT_FAILURE);
+	//if (list == NULL)
+		//exit(EXIT_FAILURE);
 	com = ft_lst_to_coms(list, status);
 	ft_free_list(list);
 	ft_countredir(com, status);
