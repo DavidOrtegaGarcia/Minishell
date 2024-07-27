c9c64a2e89a6734d1393bdc15ed607790fdbd99e
Ricardo Pocaterra Meza <rpocater@cbr12s4.42barcelona.com>
2024-07-27 14:27:18 +0200

    commented some print_commands for convenience

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: daortega <daortega@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/24 14:01:58 by daortega          #+#    #+#             */
-/*   Updated: 2024/07/27 13:17:59 by rpocater         ###   ########.fr       */
+/*   Updated: 2024/07/27 14:19:49 by rpocater         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,7 +34,7 @@ int	main(int argc, char *argv[], char *env[])
 		if (line[0] != '\0')
 			add_history(line);
 		com = ft_token_and_parse(line, &status);
-		print_commands(com);
+		//print_commands(com);
 		//EXPANSOR
 		expansor(com, l_env, status);
 		//print_commands(com);
