9ccd5e89f883b2d592c631368ce1aa23dd2b7306
David Ortega Garcia <daortega@car12s3.42barcelona.com>
2024-08-01 17:44:58 +0200

    Daily commit

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: daortega <daortega@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/24 14:01:58 by daortega          #+#    #+#             */
-/*   Updated: 2024/07/29 16:08:56 by rpocater         ###   ########.fr       */
+/*   Updated: 2024/08/01 16:22:34 by daortega         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,18 +32,14 @@ int	main(int argc, char *argv[], char *env[])
 		if (line[0] != '\0')
 			add_history(line);
 		com = ft_token_and_parse(line, &status);
-		print_commands(com);
 		//EXPANSOR
 		expansor(com, l_env, status);
-		//print_commands(com);
 		//EXEC
 		heredoc(com);
 		execute(com, l_env, env, &status);
 		//clean_heredoc(com);
 		ft_free_coms(com);
-		//free(line);
 		line = readline("minishell$ ");
 	}
-	//exit(EXIT_SUCCESS);
 	return (exit(EXIT_SUCCESS), 0);
 }
